f43e52d73dc65a8edbaf651202260d31be42b96b
ErickAraujodC <erickbyborges@gmail.com>
2023-01-16 21:01:07 -0300

    Tipos de Variaveis 4

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -10,6 +10,10 @@ public class TiposVariaveis {
         float pi = 3.14F;
         double salario = 1300.50;
 
+
+        ////
+
+        
         //Java é fortemente tipada, sendo assim, se eu tenho um n° do tipo short 
         //crio uma var a partir dele do tipo int e em seguida crio outra var do tipo short
         //a partir dessa do tipo int que foi criada do tipo short ele dará erro, ilustrando a baixo;
@@ -21,5 +25,19 @@ public class TiposVariaveis {
         // no caso acima posso trocar adicionar um cast na variavel numeroCurto2
         //O cast é uma conversão, é quando convertemos um valor de um tipo para outro tipo
 
+
+        ////
+
+
+        /*Para criar constantes usamos a palavra "final" no inicio da declaraçao da variavel,
+        *e o nome da variavel deve estar escrita em uppercase por convensao.
+        *Exemplo a baixo.
+        */
+
+        final double PI = 3.14;
+        //PI = 3.12;
+        
+        //Se eu tento alterar gera um erro dizendo que
+        //A variável local final PI não pode ser atribuída.
     }
 }
