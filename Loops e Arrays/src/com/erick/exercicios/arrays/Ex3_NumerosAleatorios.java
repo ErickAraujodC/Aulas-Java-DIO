56a39416a5260ba9df70601882b6ab7f578c78f2
ErickAraujodC <erickbyborges@gmail.com>
2023-01-21 17:58:03 -0300

    ex 3

---
+++ Loops e Arrays/src/com/erick/exercicios/arrays/Ex3_NumerosAleatorios.java
@@ -0,0 +1,35 @@
+package com.erick.exercicios.arrays;
+
+import java.util.Random;
+
+/*
+Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) armazene-os num vetor.
+Ao final mostre os números e seus sucessores.
+*/
+
+public class Ex3_NumerosAleatorios {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+
+        int [] numeros = new int[20];
+        int cont = 0;
+        
+        for(int i = 0; i < numeros.length; i++){
+            int numeroAleatorio = random.nextInt(100);
+            numeros[i] = numeroAleatorio;
+        }
+
+        System.out.print("Vetor:    ");
+        for(int numero : numeros){
+            System.out.print(numero + " ");
+        }
+
+        System.out.println(" ");
+        System.out.print("Sucessor: ");
+        do {
+            System.out.print((numeros[cont]+1) + " ");
+            cont++;
+        } while (cont<numeros.length);
+    }    
+}
