3dbe1fb7049e7168c3c30eed120f01d79dfc7e04
ErickAraujodC <erickbyborges@gmail.com>
2023-01-24 23:56:32 -0300

    metodos e classes

--- POO/src/com/erick/classes/exercicios/Carro.java
+++ POO/src/com/erick/classes/exercicios/Carro.java
@@ -1,5 +1,57 @@
 package com.erick.classes.exercicios;
 
 public class Carro {
+    String cor;
+    String modelo;
+    String montadora;
+    int capacidadeTanque;
+
+    Carro(String cor, String modelo, String montadora, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    //set e get
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+    
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+    
+    void setMontador(String montadora){
+        this.montadora = montadora;
+    }
+    
+    String setMontador(){
+        return montadora;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque(){
+        return capacidadeTanque;
+    } 
+
+    //
+
+    double totalValorTanque(double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+    }
+    
     
 }
