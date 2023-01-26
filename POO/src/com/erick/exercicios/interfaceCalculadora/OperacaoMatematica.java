328f5460130ae996919f206bd92459face2eaec3
ErickAraujodC <erickbyborges@gmail.com>
2023-01-25 23:14:31 -0300

    interface

---
+++ POO/src/com/erick/exercicios/interfaceCalculadora/OperacaoMatematica.java
@@ -0,0 +1,12 @@
+package com.erick.exercicios.interfaceCalculadora;
+
+public interface OperacaoMatematica {
+
+    void somar(double operando1, double operando2);
+
+    void subtrair(double operando1, double operando2);
+    
+    void multiplicar(double operando1, double operando2);
+    
+    void dividir(double operando1, double operando2);
+}
